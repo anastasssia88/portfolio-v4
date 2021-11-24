deeb8e592e27f63f2be785769c429153828b243d
Anastasia Titova <nastyatitova3538@gmail.com>
2021-11-24 09:49:05 -0800

    hopefully fixed the form

--- src/layouts/Form.js
+++ src/layouts/Form.js
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
-import {
-  useNetlifyForm,
-  NetlifyFormProvider,
-  NetlifyFormComponent,
-  Honeypot,
-} from "react-netlify-forms";
+
+// components
 import { Container } from "./Wrappers";
 
 const Form = () => {
@@ -18,58 +14,66 @@ const Form = () => {
   } = useForm();
   const [sent, setSent] = useState(false);
 
-  const netlify = useNetlifyForm({
-    name: "contact-form",
-    // action: '/thanks',
-    honeypotName: "bot-field",
-    onSuccess: (response, context) => {
-      console.log("Successfully sent form data to Netlify Server");
-    },
-  });
-
-  const onSubmit = (data, e) => {
-    netlify.handleSubmit(null, data);
-    reset();
-    setSent(true);
+  const handleSubmit = (data, e) => {
+    e.preventDefault();
+    console.log("Sending request with form data ...");
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": "contact-form",
+        ...data,
+      }),
+    })
+      .then((response) => {
+        reset();
+        setSent(true);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <FormContainer id="contact">
       <h2>Let's talk</h2>
-      <NetlifyFormProvider {...netlify}>
-        <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)}>
-          <Honeypot />
-          <Grid>
-            <div>
-              <label htmlFor="name">Name*</label>
-              <input id="name" {...register("name", { required: true })} />
-              {errors.name && <Error>It shouldn't be empty</Error>}
-            </div>
+      <form
+        name="contact-form"
+        onSubmit={handleSubmit}
+        method="POST"
+        action="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <div>
+          <label htmlFor="name">Name*</label>
+          <input id="name" {...register("name", { required: true })} />
+          {errors.name && <Error>It shouldn't be empty</Error>}
+        </div>
 
-            <div>
-              <label htmlFor="email">Email*</label>
-              <input
-                id="email"
-                {...register("email", {
-                  required: "It shouldn't be empty",
-                  pattern: {
-                    value:
-                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i,
-                    message: "Oops! Incorrect email",
-                  },
-                })}
-              />
-              {errors.email && <Error>{errors.email.message}</Error>}
-            </div>
+        <div>
+          <label htmlFor="email">Email*</label>
+          <input
+            id="email"
+            {...register("email", {
+              required: "It shouldn't be empty",
+              pattern: {
+                value:
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i,
+                message: "Oops! Incorrect email",
+              },
+            })}
+          />
+          {errors.email && <Error>{errors.email.message}</Error>}
+        </div>
 
-            <div>
-              <label htmlFor="message">Message</label>
-              <textarea id="message" {...register("message")} />
-            </div>
-            <StyledSubmit type="submit">Submit</StyledSubmit>
-          </Grid>
-        </NetlifyFormComponent>
-      </NetlifyFormProvider>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea id="message" {...register("message")} />
+        </div>
+        <StyledSubmit type="submit">Submit</StyledSubmit>
+      </form>
       {sent && <p>Thanks! 🙌🏼 Talk to you soon</p>}
     </FormContainer>
   );
@@ -92,58 +96,55 @@ const FormContainer = styled(Container)`
       padding: 1.5rem 1rem;
     }
   }
-`;
 
-const Grid = styled.section`
-  padding: 3rem 0;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 0.5rem 2rem;
-  grid-auto-rows: minmax(auto, auto);
-
-  div:first-child {
-    grid-column: 1 / 3;
-    grid-row: 1;
-    @media (max-width: 768px) {
-      grid-column: 1;
-      grid-row: 1;
-    }
-  }
-
-  div:nth-child(2) {
-    grid-column: 3 / 5;
-    grid-row: 1;
-    @media (max-width: 768px) {
-      grid-column: 1;
-      grid-row: 2;
-    }
-  }
-
-  div:nth-child(3) {
-    grid-column: 1 / 4;
-    grid-row: 2;
-    @media (max-width: 768px) {
-      grid-column: 1;
-      grid-row: 3;
-    }
-  }
-
-  div:last-child {
-    grid-column: 4 / 5;
-    grid-row: 2;
-    @media (max-width: 768px) {
-      grid-column: 1;
-      grid-row: 4;
-    }
-  }
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(1, auto);
-    gap: 0.5rem 1rem;
+  > form {
+    padding: 3rem 0;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 0.5rem 2rem;
     grid-auto-rows: minmax(auto, auto);
+
+    div:first-child {
+      grid-column: 1 / 3;
+      grid-row: 1;
+      @media (max-width: 768px) {
+        grid-column: 1;
+        grid-row: 1;
+      }
+    }
+    div:nth-child(2) {
+      grid-column: 3 / 5;
+      grid-row: 1;
+      @media (max-width: 768px) {
+        grid-column: 1;
+        grid-row: 2;
+      }
+    }
+    div:nth-child(3) {
+      grid-column: 1 / 4;
+      grid-row: 2;
+      @media (max-width: 768px) {
+        grid-column: 1;
+        grid-row: 3;
+      }
+    }
+    div:last-child {
+      grid-column: 4 / 5;
+      grid-row: 2;
+      @media (max-width: 768px) {
+        grid-column: 1;
+        grid-row: 4;
+      }
+    }
+
+    @media (max-width: 768px) {
+      grid-template-columns: repeat(1, auto);
+      gap: 0.5rem 1rem;
+      grid-auto-rows: minmax(auto, auto);
+    }
   }
 
-  > div {
+  > form div {
     display: flex;
     flex-direction: column;
   }
