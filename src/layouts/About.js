20c531c99179bcc68644902acc020856f474bd68
Anastasia Titova <nastyatitova3538@gmail.com>
2021-12-01 07:59:56 -0800

    move about data to separate jsons

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -2,95 +2,20 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-// import Container from "./wrappers/Container";
+import toolsJSON from "../data/tools.json";
+import experienceJSON from "../data/experience.json";
+import projectsJSON from "../data/projects.json";
+
 import { Container } from "./Wrappers";
 import Card from "../components/Card";
 import WorkCard from "../components/WorkCard";
 import Project from "../components/Project";
 
 const About = () => {
-  const [tools, setTools] = useState([
-    {
-      id: 1,
-      type: "Languages",
-      content: "Javascript, Python, HTML5, CSS3, SQL",
-    },
-    {
-      id: 2,
-      type: "Tools",
-      content: "React.js, Next.js, Flask, Django, Postgres, MongoDB, Git",
-    },
-    {
-      id: 3,
-      type: "Other",
-      content: "Text-to-speech, Voice Building, Machine learning",
-    },
-  ]);
+  const [tools, setTools] = useState(toolsJSON);
+  const [experience, setExperience] = useState(experienceJSON);
+  const [projects, setProjects] = useState(projectsJSON);
 
-  const [experience, setExperience] = useState([
-    {
-      id: 11,
-      state: "current",
-      title: "Siri Voice Buidler / Frontend Engineer",
-      dates: "Aug 2021 - Present",
-      company: "Apple via Advantis",
-      description:
-        "Improving Russian voices of Siri. Helping increase TTS team's productivity by building user interfaces for voice building tools.",
-      tags: [
-        "Python",
-        "JavaScript",
-        "Flask",
-        "Postgres",
-        "React.js",
-        "Machine learning",
-        "Phonetics",
-        "IPA",
-        "Git",
-      ],
-    },
-    {
-      id: 12,
-      state: "past",
-      title: "Frontend Engineer",
-      dates: "Jan 2019 - Aug 2021",
-      company: "Freelance",
-      description:
-        "Developed performant and accessible user interfaces with Reactjs and Nextjs. Reduced by 20% cost-per-lead by investigating and improving the performance of a website by eliminating render-blocking CSS and JavaScript. Increased  development productivity by 2 hours/day by refactoring & buidling reusable components.",
-      tags: [
-        "JavaScript",
-        "HTML/CSS",
-        "React.js",
-        "Next.js",
-        "TailwindCSS",
-        "Bootstrap",
-        "Styled Components",
-        "Git",
-      ],
-    },
-  ]);
-
-  const [projects, setProjects] = useState([
-    {
-      id: 111,
-      name: "petspaw",
-      title: "PetsPaw",
-      type: "Web app",
-      description:
-        "Individual project. Built a React web application based on The Dogs API.",
-      tags: ["React.js", "Styled Components", "API"],
-      demoLink: "https://petspaw-by-anastasia-titova.netlify.app/",
-    },
-    {
-      id: 122,
-      name: "oxana",
-      title: "Oxana's Cakes",
-      type: "Website",
-      description:
-        "Website for a local small business - bakery in Los Angeles.",
-      tags: ["JavaScript", "HTML/CSS"],
-      demoLink: "https://oxanascakes.netlify.app/",
-    },
-  ]);
   return (
     <>
       <Skills id="skills">
