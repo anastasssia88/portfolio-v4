247b9adb29b3f3dbbb65f353acfd5934a51b6600
Anastasia Titova <nastyatitova3538@gmail.com>
2022-01-31 07:17:19 -0800

    update tests

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -10,30 +10,10 @@ describe("Button component", () => {
     const buttonElement = screen.getByText("test button");
     expect(buttonElement).toBeInTheDocument();
   });
+
+  test("button is enabled", () => {
+    render(<Button content="Click me" />);
+    const button = screen.getByRole("button", { name: "Click me" });
+    expect(button).toBeEnabled();
+  });
 });
-
-// OLD
-// import React from "react";
-// import { render, unmountComponentAtNode } from "react-dom";
-// import { act } from "react-dom/test-utils";
-
-// import Button from "./Button";
-
-// let container = null;
-// beforeEach(() => {
-//   container = document.createElement("div");
-//   document.body.appendChild(container);
-// });
-
-// afterEach(() => {
-//   unmountComponentAtNode(container);
-//   container.remove();
-//   container = null;
-// });
-
-// it("button renders w/ or w/out content", () => {
-//   act(() => {
-//     render(<Button content="My Button!" />, container);
-//   });
-//   expect(container.textContent).toBe("My Button!");
-// });
